ef8cf9f146e42ee6ae1ba47e29afc7c11bffcc48
Harshit Gupta <harshit11541@gmail.com>
2017-10-07 10:15:09 +0530

    Create KMP.cpp

---
+++ KMP.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// LPS = Longest Propre Prefix Subsequence which is also a suffix
+const int N = 10000;
+int lps[N];
+
+void LPS(string pattern)
+{
+    int m = pattern.length();
+    lps[0] = 0;
+    int i = 1;
+    int len = 0;
+    while(i < m) {
+        if(pattern[i] == patern[len]) {
+            lps[i] = ++len;
+            i ++;
+        }
+        else {
+            if(len != 0) {
+                len = lps[len - 1];
+            }
+            else {
+                lps[i] = 0;
+                i ++;
+            }
+        }
+    }
+}
+
+void KMP(string text, string pattern) 
+{
+    int m = pattern.length();
+    int n = text.lenth();
+    
+    LPS(pattern);
+
+    int i = 0, j = 0;
+    while(i < n) {
+        if(pattern[j] == text[i]) {
+            i ++;
+            j ++;
+        }
+        if(j == m) {
+            cout << i - j << " ";
+            j = lps[j - 1];
+        } 
+        else if(i < n and pattern[j] != text[i]) {
+            if(j != 0) {
+                j = lps[j - 1];
+            }
+            else {
+                i ++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    string pattern, text;
+    cin >> text >> pattern;
+    KMP(text, pattern);
+    return 0;
+}
