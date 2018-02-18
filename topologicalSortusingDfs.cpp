d9fc0e042f9267e5c35c314ca5817b98c5b1199f
Harshit Gupta <harshit11541@gmail.com>
2018-02-18 11:28:43 +0530

    Create topologicalSortusingDfs.cpp

---
+++ topologicalSortusingDfs.cpp
@@ -0,0 +1,39 @@
+vector < int > v[100];
+bool visited[100];
+stack < int > s;
+
+void dfs(int n)
+{
+	visited[n] = 1;
+	for(auto x: v[n]) {
+		if(!visited[x]) {
+			dfs(x);
+		} 
+	}
+	s.push(n);
+}
+
+int main()
+{ 
+    fast;
+   	int n, m;
+   	cin >> n >> m;
+   	for(int i = 0; i < m; i ++) {
+   		int x, y;
+   		cin >> x >> y;
+   		v[x].push_back(y);
+   	}
+
+   	for(int i = 0; i < n; i ++) {
+   		if(!visited[i]) {
+   			dfs(i);
+   		}
+   	}
+
+   	while(!s.empty()) {
+   		cout << s.top() << " ";
+   		s.pop();
+   	}
+   cout << "\n";
+   return 0;
+}
