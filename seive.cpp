cee367a981b30c8a3803b642d5a7b036ef700bd1
Harshit Gupta <harshit11541@gmail.com>
2018-04-19 09:54:06 +0530

    Update seive.cpp

--- seive.cpp
+++ seive.cpp
@@ -1,44 +1,43 @@
                                         /* Normal Seive */
-const int N = 87000000;
-bool mark[N];
-int prime[N];
+const int NN = 87000000;
+bool mark[NN];
+vector < int > prime;
 
 void seive()
 {
-    int o = 0;
-    for(int i = 2; i * i <= 87000000; i ++) {
+    for(int i = 2; i * i <= NN; i ++) {
         if(mark[i] == 0) {
-            for(int j = i * 2; j <= 87000000; j += i) {
+            for(int j = i * 2; j <= NN; j += i) {
                 mark[j] = 1;
             }
         }
     }
-    for(int i = 2; i <= 87000000; i ++) {
+    for(int i = 2; i <= NN; i ++) {
         if(mark[i] == 0) {
-            prime[o ++] = i;
+            prime.push_back(i);
         }
     }
 }
 
 =============================================================================================================================================
                             /* Simple Sieve with some optimization Avoiding all even numbers */
-bool mark[N / 2];
-int prime[N];
+const int NN = 87000000;
+bool mark[NN / 2];
+vector < int > prime;
 
 void seive()
 {
-    int o = 0;
-    prime[o ++] = 2;
-    for(int i = 3; i * i < 87000000; i += 2) {
+    prime.pb(2);
+    for(int i = 3; i * i < NN; i += 2) {
         if(mark[i / 2] == 0) {
-            for(int j = i * i; j < 87000000; j += i * 2) {
+            for(int j = i * i; j < NN; j += i * 2) {
                 mark[j / 2] = 1;
             }
         }
     }
-    for(int i = 3; i <= 87000000; i +=2) {
+    for(int i = 3; i <= NN; i +=2) {
         if(mark[i / 2] == 0) {
-            prime[o ++] = i;
+            prime.push_back(i);
         }
     }
 }
