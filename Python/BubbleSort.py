6715a227e74be1009f29572e10e79ea8bfefc661
Sarah Luiz <sarah.luiz@hotmail.com>
2019-10-22 23:22:17 -0300

    Add BubbleSort in Python

---
+++ Python/BubbleSort.py
@@ -0,0 +1,7 @@
+def BubbleSort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        for j in range(i+1, n):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
